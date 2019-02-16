55c96f1751e62915152ec87da7b297cb8e758c3e
Prudence Codjia <pcodjia@ironguard.net>
2019-02-15 21:24:09 -0500

    issue

---
+++ client/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function heroBox() {
+    return ( 
+        <div id = "title" >
+        <h1>  Google Books Search </h1> <
+        <h4> Search for and Save Books </h4>
+        </div>
+    )
+}
+
+export default title;
